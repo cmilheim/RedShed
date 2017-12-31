616e7f1c44ff82da731c6cd6a3bb988e7606c11b
Chris Milheim <william.milheim@homes.com>
2017-12-30 21:34:07 -0500

    Final updates?

--- imemail/imEmailForm.php
+++ imemail/imEmailForm.php
@@ -9,8 +9,8 @@ if(substr(basename($_SERVER['PHP_SELF']), 0, 11) == "imEmailForm") {
 	if(@$_POST['action'] != 'check_answer') {
 		if(!isset($_POST['imJsCheck']) || $_POST['imJsCheck'] != 'E15C92680F3081D1572ACF6D7DE6D9E1' || (isset($_POST['imSpProt']) && $_POST['imSpProt'] != ""))
 			die(imPrintJsError());
-		$form->mailToOwner($_POST['email'] != "" ? $_POST['email'] : 'pflanz@pflanz.com', 'pflanz@pflanz.com', '', '', false);
-		$form->mailToCustomer('pflanz@pflanz.com', $_POST['email'], 'Thanks,  I have received your email', 'I will be replying soon.
+		$form->mailToOwner($_POST['email'] != "" ? $_POST['email'] : 'theredshedtally@gmail.com', 'theredshedtally@gmail.com', '', '', false);
+		$form->mailToCustomer('theredshedtally@gmail.com', $_POST['email'], 'Thanks,  I have received your email', 'I will be replying soon.
 
 But if your query was for ordering, please give us a call instead of emailing.', false);
 		@header('Location: ../index.html');
